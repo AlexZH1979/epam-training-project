18229dee6d91349befcaabbe74c629aee5d29bf9
zhmyd1979 <zhmyd1979@gmail.com>
2014-10-27 11:51:30 +0000

    update structure, add to WEB-INF  next folder: config, views

---
+++ trunk/hottel-system/src/main/java/ru/yandex/zhmyd/hotelmodel/service/impl/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package ru.yandex.zhmyd.hotelmodel.service.impl;
+
+import ru.yandex.zhmyd.hotelmodel.domain.User;
+import ru.yandex.zhmyd.hotelmodel.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+	public User loadUserByCredentials(String login, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public User getUserByID(Long userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
