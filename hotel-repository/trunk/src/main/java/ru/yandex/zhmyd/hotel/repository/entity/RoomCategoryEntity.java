52f5ab137ae5acf5e15f79131d085f03755a4123
zhmyd1979@gmail.com <zhmyd1979@gmail.com@791b3d79-c64e-b9f7-27e3-47e8ea44ae6f>
2014-12-19 10:45:23 +0000

    Удалил старые версии.... всеравно никто этого не читает

--- hotel-repository/trunk/src/main/java/ru/yandex/zhmyd/hotel/repository/entity/RoomCategoryEntity.java
+++
@@ -1,9 +0,0 @@
-package ru.yandex.zhmyd.hotel.repository.entity;
-
-/*
-* WARNING: don't change, this order used as key in db
- */
-@SuppressWarnings("unused")
-public enum RoomCategoryEntity {
-	ECONOMY, BUSINESS, ELITE
-}
