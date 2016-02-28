6e2dbf62d9867b6b33c744ab882db5e09dde4f6c
Aliaksandr <zhmyd@yandex.ru>
2016-02-29 00:07:31 +0300

    Initial commit

--- hotel/jdbc/src/main/java/com/epam/zhmyd/JDBCUtil.java
+++ hotel/jdbc/src/main/java/com/epam/zhmyd/JDBCUtil.java
@@ -3,11 +3,9 @@ package com.epam.zhmyd;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Properties;
+import java.sql.*;
+import java.sql.Date;
+import java.util.*;
 
 public class JDBCUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/";
@@ -17,11 +15,48 @@ public class JDBCUtil {
 
     private static Connection connection;
 
-    public static void execute(String sql) {
+    public static void executeStatement(String sql) {
         Connection connection = getConnection();
         if (connection != null) {
             try (Statement statement = connection.createStatement()) {
                 statement.execute(sql);
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    private static void fillFriendships(String sql, int userId1, int userId2, Date date) {
+        if (connection != null) {
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setInt(1, userId1);
+                statement.setInt(2, userId2);
+                statement.setDate(3, date);
+                statement.execute();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    private static void fillPosts(String sql, int userId1, String text, Date date) {
+        if (connection != null) {
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setInt(1, userId1);
+                statement.setString(2, text);
+                statement.setDate(3, date);
+                statement.execute();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static void fillUser(String sql, String name, String surname, Date date) {
+        if (connection != null) {
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setString(1, name);
+                statement.setString(2, surname);
+                statement.setDate(3, date);
+                statement.execute();
             } catch (SQLException e) {
                 System.out.println(e.getMessage());
             }
@@ -48,8 +83,36 @@ public class JDBCUtil {
     }
 
     public static void create() {
-        execute("CREATE SCHEMA `simple`;");
-        execute("use `simple`;");
-        execute(SQLStatements.TABLE_USER);
+        executeStatement(SQLStatements.DROP_SHEMA);
+        executeStatement(SQLStatements.CREATE_SHEMA);
+        executeStatement("use `simple`;");
+        executeStatement(SQLStatements.TABLE_USER);
+        executeStatement(SQLStatements.TABLE_FRIENSHIPS);
+        executeStatement(SQLStatements.TABLE_POSTS);
+
+        for(int i =0; i< 1001;i++){
+            fillUser(SQLStatements.INSERT_USER, "user"+i, "user"+i,new Date(System.currentTimeMillis()));
+        }
+
+        for(int i =1; i< 500;i++){
+            int count = new Random().nextInt(200);
+            List<Integer> friends = new ArrayList<>();
+            for (int j = 0; j <count ; j++) {
+                int friend = new Random().nextInt(500)+500;
+                if(!friends.contains(friend)) {
+                    fillFriendships(SQLStatements.INSERT_FRIENSHIPS, i, friend, new Date(System.currentTimeMillis()));
+                    friends.add(friend);
+                }
+            }
+        }
+        for(int i =1; i< 1001;i++){
+            int count = new Random().nextInt(100);
+            for (int j = 0; j <count ; j++) {
+                    fillPosts(SQLStatements.INSERT_POSTS, i, "HELLO", new Date(System.currentTimeMillis()));
+
+            }
+        }
+
+
     }
 }
